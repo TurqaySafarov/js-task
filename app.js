34c77474f300ed840dd2b3af6de4a4ba7d5899e0
TurqaySafarov <161557826+TurqaySafarov@users.noreply.github.com>
2024-04-03 15:29:43 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,118 @@
+
+//TASK
+
+// 1. mehsullarin ucuzdan bahaya siralanmasi
+// let products = [
+//   {
+//     id: 1,
+//     name: "Iphone 14 Pro",
+//     price: 900,
+//   },
+//   {
+//     id: 2,
+//     name: "BMW",
+//     price: 20000,
+//   },
+//   {
+//     id: 3,
+//     name: "Samsung A70",
+//     price: 600,
+//   },
+//   {
+//     id: 4,
+//     name: "Iphone 15 Pro Max", // iphone 15 pro max
+//     price: 1500,
+//   },
+// ];
+// products.sort((a, b) => a.price - b.price);
+// products.forEach((product) => {
+//   console.log(`${product.name} ${product.price}`);
+// });
+
+
+//?############################################################################################
+
+//2. Beş random eded yaradib arraya yigin, sonda ise onlarin cemini gosterin
+// let numbers=[]
+// let sum=0
+// for (let i=1; i<=5; i++){
+// const random=Math.floor(Math.random()*100)
+// numbers.push(random)
+// }
+// console.log(numbers);
+// numbers.forEach(function(num) {
+//     sum += num;
+// });
+// console.log(sum);
+
+//?############################################################################################
+
+//3. Foreach arasdirin, map ve normal for dongusu ile ferqi nedi ona baxin
+
+// const forEach = [1, 2, 3, 4, 5];
+
+// forEach.forEach(function(element) {
+//   console.log(element);
+// });
+// forEach  hər bir elementi üçün verilmiş funksiyani çağırmağa xidmət edən bir dizi metodudur. Bu metodu, bir dizi içindəki hər bir elementi təkrar etmək üçün istifadə edə bilərik.
+// foreach arrey daxilindəki elementleri bir bir cagirir.
+// Map, arrey icindeki her bir element uzerinde eyni emeliyyati aparir.
+
+
+
+//?##################################################################################################################################################################################################3
+
+//4. Json, fetch, api bunlar nedir nece isleyir
+// JSON (JavaScript Object Notation): JSON, verilənlərin özünü təsvir etmək üçün istifadə olunan bir formatdır. 
+// Bu format sadə, oxunaqlı və insanlar və proqramlar arasında məlumat mübadiləsini asanlaşdırır. 
+// JSON formatında verilən məlumat obyekt və dizilərdən ibarətdir və məlumatın təsvir edilməsi üçün açıq bir standartdır.
+
+// Fetch API: Fetch API, brauzerlər üçün HTTP tələblərini göndərmək və qəbul etmək üçün modern bir JavaScript interfeysidir.
+//  Bu API, XHR (XMLHttpRequest) obyektinə alternativ olaraq dəyişkən və asinqron şəkildə HTTP tələblərinin göndərilməsini təmin edir. 
+//  Fetch API, promise əsasında işlədiyi üçün məlumatlar göndərildikdə və qəbul edildikdə bu prosesləri asanlaşdırır.
+
+// API (Application Programming Interface): Bir proqram tərəfindən təmin edilən xidmətlərin, funksiyaların və ya məlumatların digər proqramlar tərəfindən istifadə 
+// edilməsi üçün təyin olunmuş interfeysdir. İnternetdə, "API" geniş istifadə olunan bir tərəfdaşlıq və integrasiya mexanizmidir. Tətbiqat proqramlaşdırma interfeysi,
+//  bir proqramın funksiyalarını və xidmətlərini başqa proqramlar tərəfindən istifadə etmək üçün nəzərdə tutulmuş formadır.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ?###############################################################################################################
+
+
+
+
+
+
+
+
